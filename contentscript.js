dd8e29cf376a11fce6118687e40b2a1f2a3abcd1
Nicole Bieber <nbieber@amplify.com>
2014-06-13 22:55:39 -0400

    display text based on URL

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,33 @@
+var characterText = [
+	{
+		"urls": ["google"],
+		"text": "You're on google!"
+	},
+	{
+		"urls": ["twitter"],
+		"text": "You're on Twitter!"
+	}
+]
+
+function getMatchText() {
+	//TODO put characterText in separate json file
+    for(var i = 0; i < characterText.length; i++)
+    {
+    	var matchObject = characterText[i];
+    	for(var j = 0; j < matchObject.urls.length; j++)
+    	{
+    		var url = matchObject.urls[j];
+    		if(location.hostname.indexOf(url) > -1)
+    		{
+    			return matchObject.text;
+    		}
+    	}
+    }
+}
+
 jQuery(document).ready(function($) {
     $("body").append("<div id='character'></div>");
+
+    alert(getMatchText());
 });
+
