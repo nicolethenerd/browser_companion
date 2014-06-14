523dd29ad99ea525415fb6813aa465d7f8881807
lpatmo <linda.peng@alumni.duke.edu>
2014-06-14 12:03:14 -0400

    enlarged tooltip

--- contentscript.js
+++ contentscript.js
@@ -6,6 +6,10 @@ var characterText = [
 	{
 		"urls": ["twitter"],
 		"text": "You're on Twitter!"
+	},
+	{
+		"urls": ["reddit"],
+		"text": "OMG YOU ARE ON REDDIT I AM ONLY WRITING THIS SENTENCE TO TEST HOW THE TOOLTIP LOOKS LIKE LOREM IPSUM IPSUM"
 	}
 ]
 
