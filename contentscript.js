90bfb318c7e9ac73cb96cd388403577287d87190
Nicole Bieber <nbieber@amplify.com>
2014-06-13 22:40:24 -0400

    add css

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function($) {
-    $("body").append("<div style='background: blue; width: 100px; height: 100px; position: fixed; bottom: 10px; right: 10px;'></div>");
+    $("body").append("<div id='character'></div>");
 });
